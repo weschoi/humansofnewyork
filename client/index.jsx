65eb5c37c4823c88b9bd3c3d49ff86dd88c9cc2e
Wesley Choi <weschoi.creative@gmail.com>
2018-02-26 21:55:17 -0800

    implemented arrow, quote, and bottom links

--- client/index.jsx
+++ client/index.jsx
@@ -43,6 +43,17 @@ class App extends React.Component {
             </div>
 
             <div className="statue"></div>
+            <div className="arrow"></div>
+
+            <div className="quote">
+              <div>"I went to New York City to be born again."</div>
+              <div>Kurt Vonnegut</div>
+            </div>
+
+            <div className="links">
+              <a href="http://www.humansofnewyork.com/">go to the original</a>
+              <a href="https://www.amazon.com/gp/product/1250058902/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=1250058902&linkCode=as2&tag=humofnewyor-20&linkId=PDCZMIXZETZP44D7" target="_blank">purchase the book</a>
+            </div>
 
           </div>
 
